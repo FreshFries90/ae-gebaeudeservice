0b8ac1661c84f8a12f4f7802fc63b18f182569a3
Stefan Fries <41573691+FreshFries90@users.noreply.github.com>
2026-03-23 14:40:46 +0100

    Tagmanager

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { Inter } from "next/font/google";
 import { SiteHeader } from "./components/SiteHeader";
 import { SiteFooter } from "./components/SiteFooter";
 
+import Script from "next/script";
+
 const inter = Inter({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "900"],
@@ -26,6 +28,24 @@ export default function RootLayout({
   return (
     <html lang="de" className={`scroll-smooth ${inter.variable}`}>
       <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),
+                    dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MVNX7XZ8');
+            `,
+          }}
+        />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
@@ -37,6 +57,14 @@ export default function RootLayout({
       </head>
 
       <body className="bg-surface-light dark:bg-background-dark text-text-main dark:text-gray-100 font-display selection:bg-primary selection:text-white overflow-x-hidden">
+       <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MVNX7XZ8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <SiteHeader />
 
         {/* wichtig: Offset für fixed Header */}
