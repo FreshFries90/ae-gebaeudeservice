675045c1c46a6f1a1fb310bbe317fbc2bb4e1041
Stefan Fries <41573691+FreshFries90@users.noreply.github.com>
2026-03-27 17:08:23 +0100

    Baureinigung

--- app/baureinigung/page.tsx
+++ app/baureinigung/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import type { Metadata } from "next";
 import BaureinigungForm from "./BaureinigungForm";
+import { ServiceCitiesSection } from "@/components/sections/ServiceCitiesSection";
 
 const PHONE = "+491757540841";
 const PHONE_LABEL = "0175 75 40 841";
@@ -382,6 +383,7 @@ export default function Page() {
           </div>
         </div>
       </section>
+      <ServiceCitiesSection defaultService="baureinigung" />
     </>
   );
 }
